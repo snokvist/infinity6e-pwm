8d8a13865d6e429b72efa37fab8fe5eaa74fb656
snokvist <joakim.snokvist@gmail.com>
2026-02-21 10:01:27 +0100

    buildroot: build and install waybeam-pwm on target

--- infinity6e-pwm.mk
+++ infinity6e-pwm.mk
@@ -9,4 +9,13 @@ INFINITY6E_PWM_PATCH_DIR = $(BR2_EXTERNAL_GENERAL_PATH)/package/infinity6e-pwm/p
 
 LINUX_PATCHES += $(INFINITY6E_PWM_PATCH_DIR)
 
+define INFINITY6E_PWM_BUILD_CMDS
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -o $(@D)/waybeam-pwm \
+		$(BR2_EXTERNAL_GENERAL_PATH)/package/infinity6e-pwm/files/waybeam-pwm.c
+endef
+
+define INFINITY6E_PWM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/waybeam-pwm $(TARGET_DIR)/usr/bin/waybeam-pwm
+endef
+
 $(eval $(generic-package))
